4441d8877c6c2b840cb58a832ae02a2a59ed92a8
jeefies <jeefy163@163.com>
2021-11-14 10:14:58 +0800

    add rasoi os download links

---
+++ url.sh
@@ -0,0 +1,46 @@
+# rpi armhf lite
+export rpi_hf_lite='https://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os-images/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28/2021-05-07-raspios-buster-armhf-lite.zip'
+export rpi_hf_lite_sha1="$rpi_hf_lite.sha1"
+
+# rpi armhf 
+export rpi_hf='https://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os-images/raspios_armhf/images/raspios_armhf-2021-05-28/2021-05-07-raspios-buster-armhf.zip'
+export rpi_hf_sha1="$rpi_hf.sha1"
+
+# rpi armhf full
+export rpi_hf_full='https://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os-images/raspios_full_armhf/images/raspios_full_armhf-2021-05-28/2021-05-07-raspios-buster-armhf-full.zip'
+export rpi_hf_full_sha1="$rpi_hf_full.sha1"
+
+# rpi arm64
+export rpi_64='https://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os-images/raspios_arm64/images/raspios_arm64-2021-05-28/2021-05-07-raspios-buster-arm64.zip'
+export rpi_64_sha1="$rpi_64.sha1"
+
+# rpi arm64 lite
+export rpi_64_lite='https://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os-images/raspios_lite_arm64/images/raspios_lite_arm64-2021-05-28/2021-05-07-raspios-buster-arm64-lite.zip'
+export rpi_64_lite_sha1="$rpi_64_lite.sha1"
+
+# chromium os for rpi
+export chromium_os='https://github.com/FydeOS/chromium_os-raspberry_pi/releases/download/r92/chromiumos_image_r92r1-rpi4b.img.xz'
+export chromium_os_sha256='907f10a28a65ae41e015885b83bd7b59370bb087f68acffb46bc96d7870b5f53  chromiumos_image_r92r1-rpi4b.img.xz'
+
+# archlinux arm64 rootfs
+export arch_64='https://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/os/rpi/ArchLinuxARM-2021.07-rpi-aarch64-rootfs.tar.gz'
+export arch_64_md5="$arch_64.md5"
+
+# archlinux armhf rootfs
+export arch='https://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/os/rpi/ArchLinuxARM-2021.07-rpi-4-rootfs.tar.gz'
+export arch_md5="$arch.md5"
+
+# manjaro linux arm64
+export manjaro_mate='https://mirrors.tuna.tsinghua.edu.cn/osdn/storage/g/m/ma/manjaro-arm/rpi4/mate/20.12.1/Manjaro-ARM-mate-rpi4-20.12.1.img.xz'
+export manjaro_mate_sha1="$manjaro_mate.sha1"
+
+# fedora aarch64
+export fedora_soas='https://mirrors.tuna.tsinghua.edu.cn/fedora-altarch/releases/34/Spins/aarch64/images/Fedora-SoaS-34-1.2.aarch64.raw.xz'
+export fedora_lite='https://mirrors.tuna.tsinghua.edu.cn/fedora-altarch/releases/34/Spins/aarch64/images/Fedora-Minimal-34-1.2.aarch64.raw.xz'
+export fedora_checksums='https://mirrors.tuna.tsinghua.edu.cn/fedora-altarch/releases/34/Spins/aarch64/images/Fedora-Spins-34-1.2-aarch64-CHECKSUM'
+
+# epeneuler aarch64
+export openeular_ukui='https://isrc.iscas.ac.cn/eulixos/repo/others/openeuler-raspberrypi/images/openEuler-20.03-LTS-SP1-UKUI-raspi-aarch64-alpha2.img.xz'
+export openeular_ukui_sha256='https://isrc.iscas.ac.cn/eulixos/repo/others/openeuler-raspberrypi/images/openEuler-20.03-LTS-SP1-UKUI-raspi-aarch64-alpha2.img.xz.sha256sum'
+export openeular_dde='https://isrc.iscas.ac.cn/eulixos/repo/others/openeuler-raspberrypi/images/openEuler-20.03-LTS-SP1-DDE-raspi-aarch64-alpha2.img.xz'
+export openeular_dde_sha256='https://isrc.iscas.ac.cn/eulixos/repo/others/openeuler-raspberrypi/images/openEuler-20.03-LTS-SP1-DDE-raspi-aarch64-alpha2.img.xz.sha256sum'
